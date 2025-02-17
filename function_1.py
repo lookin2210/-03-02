1a48b5dcfaba0f73f929b3e3ee417eb059a34c75
lookin2210 <36889@bpk.ac.th>
2025-02-17 11:48:39 +0700

    Add files via upload

---
+++ function_1.py
@@ -0,0 +1,6 @@
+def add_numbers(A, B):
+    return A + B
+A = float(input("กรุณากรอกเลขตัวที่ 1: "))
+B = float(input("กรุณากรอกเลขตัวที่ 2: "))
+result = add_numbers(A, B)
+print(f"ผลลัพธ์การบวก {A} + {B} คือ: {result}")
